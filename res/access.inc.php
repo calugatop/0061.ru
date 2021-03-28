58cdcaf8dc92c5681603624b7cdaeaffb2771343
calugatop <50307913+calugatop@users.noreply.github.com>
2021-03-28 04:14:49 +0300

    333

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '59kn7eqm';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('sugkovmm'),
+		'id' => 'glm1890y',
+		'name' => 'Admin',
+		'password' => '6t6wt53p',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'новый пользователь' => array(
+		'groups' => array('sjflchzm'),
+		'id' => 'cysfmnr4',
+		'name' => 'Новый пользователь',
+		'password' => 'cv12xoux',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('glm1890y');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
